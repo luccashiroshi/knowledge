1041f445af25c3cbf553e3b027fc3a365bab0b66
= <luccashiroshi@gmail.com>
2022-07-07 13:58:39 -0300

    articles

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -19,4 +19,15 @@ module.exports = app => {
         .put(app.api.category.save)
         .delete(app.api.category.remove)
 
+    app.route('/articles')
+        .get(app.api.article.get)
+        .post(app.api.article.save)
+
+    app.route('/articles/:id')
+        .get(app.api.article.getById)
+        .put(app.api.article.save)
+        .delete(app.api.article.remove)
+
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory)
 }
