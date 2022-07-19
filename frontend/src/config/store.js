4601baee756619e5eaf4ca88fd097206c1381576
= <luccashiroshi@gmail.com>
2022-07-19 15:56:24 -0300

    tela de login/cadastro

--- frontend/src/config/store.js
+++ frontend/src/config/store.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -6,17 +7,29 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         isMenuVisible: false,
-        user: {
-            name: 'Usuário Mock',
-            email: 'mock@gmail.com'
-        }
+        user: null
     },
     mutations: {
         toggleMenu(state, isVisible) {
+            if(!state.user) {
+                state.isMenuVisible = false
+                return
+            }
+
             if(isVisible === undefined) {
                 state.isMenuVisible = !state.isMenuVisible
             } else {
                 state.isMenuVisible = isVisible
+            }
+        },
+        setUser(state, user) {
+            state.user = user
+            if(user) {
+                axios.defaults.headers.common['Authorization'] = `bearer ${user.token}`
+                state.isMenuVisible = true
+            } else {
+                delete axios.defaults.headers.common['Authorization']
+                state.isMenuVisible = false
             }
         }
     }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,9 +10,6 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-// temporario
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikx1Y2NhcyBIaXJvc2hpIiwiZW1haWwiOiJsdWNjYXNoaXJvc2hpQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NTgxNjc2NjMsImV4cCI6MTY1ODQyNjg2M30.HeClArMz0HRItcb6k1LiWOSSp5EU1WGDskb0zt6yMyo'
-
 new Vue({
   store,
   router,
