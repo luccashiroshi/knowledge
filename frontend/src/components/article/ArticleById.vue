a116248e5dee47f191c5b65af8299da5b33e6d4d
= <luccashiroshi@gmail.com>
2022-07-19 16:58:59 -0300

    auth expired token

--- frontend/src/components/article/ArticleById.vue
+++ frontend/src/components/article/ArticleById.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="article-by-id">
-    <PageTitle icon="fa fa-file-o" :main="article.name" :sub="article.description"/>
+    <PageTitle
+      icon="fa fa-file-o"
+      :main="article.name"
+      :sub="article.description"
+    />
     <div class="article-content" v-html="article.content"></div>
   </div>
 </template>
 
 <script>
+import "highlightjs/styles/dracula.css";
+import hljs from "highlightjs/highlight.pack.js";
 import { baseApiUrl } from "@/global";
 import axios from "axios";
 import PageTitle from "../template/PageTitle.vue";
@@ -22,27 +28,32 @@ export default {
     const url = `${baseApiUrl}/articles/${this.$route.params.id}`;
     axios.get(url).then((res) => (this.article = res.data));
   },
+  updated() {
+    document.querySelectorAll(".article-content pre").forEach((e) => {
+      hljs.highlightBlock(e);
+    });
+  },
 };
 </script>
 
 <style>
 .article-content {
-    background-color: #FFF;
-    border-radius: 8px;
-    padding: 25px;
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 25px;
 }
 .article-content pre {
-    padding: 20px;
-    border-radius: 8px;
-    font-size: 1.2rem;
-    background-color: #282a36;
-    color: #FFF;
+  padding: 20px;
+  border-radius: 8px;
+  font-size: 1.2rem;
+  background-color: #282a36;
+  color: #fff;
 }
 .article-content img {
-    max-width: 100%;
+  max-width: 100%;
 }
 
 .article-content :last-child {
-    margin-bottom: 0px;
+  margin-bottom: 0px;
 }
 </style>
